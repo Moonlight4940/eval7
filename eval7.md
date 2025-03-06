034cbf9d7819cb9c69fb1bafb04ed17270497630
Moonlight4940 <alex4940@hotmail.com>
2025-03-06 22:23:48 +0100

    debut

---
+++ eval7.md
@@ -0,0 +1,43 @@
+Enoncé
+
+Sur Symfony, nous souhaitons créer une interface permettant de gérer les données d’une entreprise. 
+Pour réaliser ce projet, nous devrons utiliser les bonnes techniques du framework (route, controller, orm, vue, formulaire, datafixture ...).
+
+1 Créer une base de données nommé "entreprise". (sur SQLITE ou MySQL à votre convenance)  1 point
+
+2 Créer une table nommée "employe" avec les champs suivants :  2 points
+
+ id 
+ prenom 
+ nom 
+ telephone 
+ email 
+ adresse 
+ poste 
+ salaire 
+_ dtnaissance
+ isactive
+
+3 Créer une table nommée "service" avec les champs suivants : 2 points
+
+ id 
+ nom 
+ description 
+_ dt_creation
+
+4 Relation entre les tables : 2 points
+
+Un employé travaille dans un et un seul service
+Un service contient un ou plusieurs salariés
+
+5 Remplissez les tables avec des données fictives. - 4 points
+
+6 Créer une interface de gestion (CRUD) pour chacune de ces tables - 3 points
+
+7 En plus des pages de gestion, le site internet devra disposer d'une page d'accueil qui fait la synthèse de l'ensemble des informations stockées en base de données. - 4 points
+
+8 Utiliser Bootstrap pour réaliser la mise en page / la barre de menu / le responsive design ... Vous avez carte blanche pour la mise en page - 2 points
+
+9 Une fois le projet finalisé, vous devez créer un dépôt github public et réaliser un push du code source
+
+10 Remplir le formulaire suivant https://forms.gle/e83c2fHyP1tMKVU87 avec votre Prénom / Nom / email et url du dépôt github
---
+++ src/Entity/Service.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ServiceRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ServiceRepository::class)]
+class Service
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $dt_creation = null;
+
+    /**
+     * @var Collection<int, Employe>
+     */
+    #[ORM\OneToMany(targetEntity: Employe::class, mappedBy: 'service')]
+    private Collection $employes;
+
+    public function __construct()
+    {
+        $this->employes = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getDtCreation(): ?\DateTimeInterface
+    {
+        return $this->dt_creation;
+    }
+
+    public function setDtCreation(\DateTimeInterface $dt_creation): static
+    {
+        $this->dt_creation = $dt_creation;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Employe>
+     */
+    public function getEmployes(): Collection
+    {
+        return $this->employes;
+    }
+
+    public function addEmploye(Employe $employe): static
+    {
+        if (!$this->employes->contains($employe)) {
+            $this->employes->add($employe);
+            $employe->setService($this);
+        }
+
+        return $this;
+    }
+
+    public function removeEmploye(Employe $employe): static
+    {
+        if ($this->employes->removeElement($employe)) {
+            // set the owning side to null (unless already changed)
+            if ($employe->getService() === $this) {
+                $employe->setService(null);
+            }
+        }
+
+        return $this;
+    }
+}
