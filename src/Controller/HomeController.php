97e61372d8c56a2e2c343ee5750a9c6b23b80441
Moonlight4940 <alex4940@hotmail.com>
2025-03-07 12:50:58 +0100

    fin fin

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,6 +7,8 @@ use App\Repository\ServiceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
+use Symfony\UX\Chartjs\Model\Chart;
 
 final class HomeController extends AbstractController
 {
