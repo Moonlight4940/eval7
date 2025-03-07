d664be5695b03ec49cd8df160de706e5a13fe899
Moonlight4940 <alex4940@hotmail.com>
2025-03-07 12:42:38 +0100

    fin

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Employe;
+use App\Repository\ServiceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,11 +12,21 @@ final class HomeController extends AbstractController
 {
     //http://127.0.0.1:8000/
     #[Route('/', name: 'app_home')]
-    public function index(): Response
-    {
+    
+    public function index(ServiceRepository $serviceRepository): Response
+{
+    $services = $serviceRepository->findAll();
+    $chartData = [];
+foreach ($services as $service) {
+    $chartData= [
+        'service' => $service->getNom(),
+        'employeeCount' => count($service->getEmployes()),
+    ];
+}
+    return $this->render('home/index.html.twig', [
         
-        return $this->render('home/index.html.twig', [
-            'home' => 'HomeController',
-        ]);
-    }
+        'chartData' => $chartData,
+        'home' => 'Bonjour!'
+    ]);
+}
 }
