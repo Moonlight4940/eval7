62066df7d2659e6c3d66601dc19e817621c2f3f8
Moonlight4940 <alex4940@hotmail.com>
2025-03-09 14:12:50 +0100

    ajustement de chart.js

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,23 +12,22 @@ use Symfony\UX\Chartjs\Model\Chart;
 
 final class HomeController extends AbstractController
 {
-    //http://127.0.0.1:8000/
     #[Route('/', name: 'app_home')]
-    
     public function index(ServiceRepository $serviceRepository): Response
-{
-    $services = $serviceRepository->findAll();
-    $chartData = [];
-foreach ($services as $service) {
-    $chartData= [
-        'service' => $service->getNom(),
-        'employeeCount' => count($service->getEmployes()),
-    ];
-}
-    return $this->render('home/index.html.twig', [
-        
-        'chartData' => $chartData,
-        'home' => 'Bonjour!'
-    ]);
-}
+    {
+        $services = $serviceRepository->findAll();
+        $chartData = []; // Initialize as an empty array
+
+        foreach ($services as $service) {
+            $chartData[] = [
+                'service' => $service->getNom(),
+                'employeeCount' => count($service->getEmployes()),
+            ];
+        }
+
+        return $this->render('home/index.html.twig', [
+            'chartData' => $chartData,
+            'home' => 'Bonjour!'
+        ]);
+    }
 }
